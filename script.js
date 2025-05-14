c84ab2ddef49362bc027a445c822431d172cad2c
Pijey2004 <bonerush03@gmail.com>
2025-05-14 13:17:47 +0800

    Added click event handlers for buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+let counter1 = 0;
+let counter2 = 0;
+const btn1 = document.getElementById("btn1").addEventListener("click", () => {
+    counter1++;
+});
+
+const btn2 = document.getElementById("btn1").addEventListener("click", () => {
+    counter2++;
+});
