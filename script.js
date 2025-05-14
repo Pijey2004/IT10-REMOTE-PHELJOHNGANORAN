ffc05529e0208b2dca813bc775b4b90f8d7a6715
Pijey2004 <bonerush03@gmail.com>
2025-05-14 13:57:52 +0800

    final fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn1 = document.getElementById("btn1");
 btn1.addEventListener("click", () => {
     counter1++;
     text1.innerText = counter1; 
-    text3.innerText = counter1 + counter2;
+    text3.innerText ="Total Count: "(counter1 + counter2);
     localStorage.setItem("counter_1", counter1);
 });
 
@@ -12,7 +12,7 @@ const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", () => {
     counter2++;
     text2.innerText = counter2; 
-    text3.innerText = counter1 + counter2;
+    text3.innerText ="Total Count: "(counter1 + counter2);
     localStorage.setItem("counter_2", counter2);
 });
 
@@ -23,7 +23,7 @@ const text3 = document.getElementById('totalCount');
 load();
 function load() {
     counter1 = Number(localStorage.getItem("counter_1"));
-    counter1 = Number(localStorage.getItem("counter_2"));
+    counter2 = Number(localStorage.getItem("counter_2"));
     text1.innerText = counter1; 
     text2.innerText = counter2;     
     text3.innerText ="Total Count: "(counter1 + counter2);
