d9983e416005a4ab2dbde54c99b95d8b052e39ff
Pijey2004 <bonerush03@gmail.com>
2025-05-14 13:28:46 +0800

    Loaded saved button click countsfrom localStorage

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ const btn2 = document.getElementById("btn1").addEventListener("click", () => {
 const text1 = document.getElementById('count1');
 const text2 = document.getElementById('count2');
 const text3 = document.getElementById('totalCount');
+
+window.onload(() => {
+    counter1 = localStorage.getItem("counter_1");
+    counter1 = localStorage.getItem("counter_2");
+    text1.innerText = counter1; 
+    text2.innerText = counter2;     
+    text3.innerText = counter1 + counter2;
+})
