9f0887c7a62ad5d78c701ab477d1c633fa0d6242
Pijey2004 <bonerush03@gmail.com>
2025-05-14 13:56:20 +0800

    implemented a fix again

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const text3 = document.getElementById('totalCount');
 
 load();
 function load() {
-    counter1 = parseInt(localStorage.getItem("counter_1"));
-    counter1 = parseInt(localStorage.getItem("counter_2"));
+    counter1 = Number(localStorage.getItem("counter_1"));
+    counter1 = Number(localStorage.getItem("counter_2"));
     text1.innerText = counter1; 
     text2.innerText = counter2;     
     text3.innerText ="Total Count: "(counter1 + counter2);
