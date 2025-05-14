4b6edbe097826d6896f4a191b3bde8de5337a3c6
Pijey2004 <bonerush03@gmail.com>
2025-05-14 13:22:41 +0800

    Displayed individual count and total counts for all buttons

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ let counter1 = 0;
 let counter2 = 0;
 const btn1 = document.getElementById("btn1").addEventListener("click", () => {
     counter1++;
+    text1.innerText = counter1; 
+    text3.innerText = counter1 + counter2;
 });
 
 const btn2 = document.getElementById("btn1").addEventListener("click", () => {
     counter2++;
+    text2.innerText = counter2; 
+    text3.innerText = counter1 + counter2;
 });
+
+const text1 = document.getElementById('count1');
+const text2 = document.getElementById('count2');
+const text3 = document.getElementById('totalCount');
